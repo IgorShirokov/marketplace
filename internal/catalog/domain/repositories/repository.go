6358bf458bbf13196b5cb0eec6056a82c8608617
IgorShirokov <shirokov-iv@mail.ru>
2026-04-01 15:50:33 +0300

    catalog: repositories

---
+++ internal/catalog/domain/repositories/repository.go
@@ -0,0 +1,19 @@
+package repositories
+
+import (
+	"context"
+
+	"github.com/IgorShirokov/marketplace/internal/catalog/domain/entities"
+)
+
+type BrandRepository interface {
+	Brand(ctx context.Context) ([]entities.Brand, error)
+}
+
+type CategoryRepository interface {
+	Categories(ctx context.Context) ([]entities.Category, error)
+}
+
+type CatalogItemRepository interface {
+	Items(ctx context.Context) ([]entities.CatalogItem, error)
+}
